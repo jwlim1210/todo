b9f186358cbc24c4ed340926af5f18068ed7d52f
임정우 <jwlim@fdxnetworks.com>
2025-02-27 18:00:52 +0900

    no message

---
+++ todo/src/main/java/com/example/fdx/todo/TodoApplication.java
@@ -0,0 +1,14 @@
+package com.example.fdx.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("est");
+		SpringApplication.run(TodoApplication.class, args);
+	}
+
+}
